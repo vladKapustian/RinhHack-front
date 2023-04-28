3edf84664425fd780d04e65ec1b6194e7d9c460f
unknown <od1nsky228@gmail.com>
2023-04-29 00:44:45 +0300

    Initial commit

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import image from "../../../public/Vector.png";
+
+const Navbar = () => {
+  return (
+    <div>
+      <Image src={image} alt="" />
+    </div>
+  );
+};
+
+export default Navbar;
